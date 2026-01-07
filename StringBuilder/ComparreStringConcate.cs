f2c6df8c1828693878ba00c042c6ea689b097684
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-07 15:10:50 +0530

    [Rishabh].Modified the code and added comments.

--- StringBuilder/ComparreStringConcate.cs
+++ StringBuilder/ComparreStringConcate.cs
@@ -1,3 +1,7 @@
+//Problem 2: Compare StringBuilder Performance
+//Problem: Write a program that compares the performance of StringBuilder for
+//concatenating strings multiple times.
+
 using System;
 using System.Diagnostics;
 using System.Text;
@@ -6,22 +10,28 @@ public class CompareStringConcate
 {
     public static void Compare()
     {
+        // number of concatination will be performed;
         const int iterations = 200000;
 
+        // a stop watch to calculate the time taken for the concatination.
         Stopwatch s1 = Stopwatch.StartNew();
         string s = "";
 
+        // looping
         for(int i = 0; i < iterations; i++)
         {
             s += 'A'; 
         }
 
+        //time stopped
         s1.Stop();
         Console.WriteLine("String Time: " + s1.ElapsedMilliseconds + " ms");
 
         Stopwatch s2 = Stopwatch.StartNew();
+        //using stringbuilder to concate.
         StringBuilder str = new StringBuilder();
 
+        //looping
         for(int i = 0; i < iterations; i++)
         {
             str.Append('A');
