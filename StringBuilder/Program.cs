84a2c985eaa05ddc627fa8ea321def67593f2528
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-07 15:14:27 +0530

    [Rishabh]. Add Program.cs to run all the programs that are in the string builder project.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //string s = Console.ReadLine() ?? "";
+        //Console.WriteLine(DuplicateRemove.RemovesDuplicateElements(s));
+
+        //string s = Console.ReadLine() ?? "";
+        //Console.WriteLine(ReverseString.reverseString(s));
+
+        //string[] str = ["rishabh", "kumar", "tiwari"];
+        //Console.WriteLine(ConcateString.Concate(str));
+
+        CompareStringConcate.Compare();
+    }
+}
