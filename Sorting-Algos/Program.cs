0a2cd5b038756bfd3e4cf533e86f69f4d19ca13a
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-07 12:01:33 +0530

    [Rishabh]. Add Program.cs to run all the sorting program in the project.

---
+++ Sorting-Algos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 8, 6, 4, 6, 2, 3, 4, 8 };
+
+        //BubbleSort.Sort(arr);
+        //InsertionSort.Sort(arr);
+        //SelectionSort.Sort(arr);
+        //MergeSort.Sort(arr);
+        //QuickSort.quickSort(arr,0,arr.Length-1);
+        //CountingSort.CountSort(arr);
+        HeapSort.HeapSorting(arr);
+
+        Console.WriteLine(string.Join(",",arr));
+    }
+}
