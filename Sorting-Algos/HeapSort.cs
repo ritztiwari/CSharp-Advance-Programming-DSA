6ba0712ad3c184d82da5407972ca4d4161e1755d
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-07 11:59:41 +0530

    [Rishabh]. Add HeapSort.cs proram to sort the given array in ascending order.

---
+++ Sorting-Algos/HeapSort.cs
@@ -0,0 +1,40 @@
+using System;
+
+internal class HeapSort
+{
+    internal static void HeapSorting(int[] arr)
+    {
+        int n = arr.Length;
+
+        // Build Max Heap
+        for (int i = n / 2 - 1; i >= 0; i--)
+            Heapify(arr, n, i);
+
+        // Extract elements one by one
+        for (int i = n - 1; i > 0; i--)
+        {
+            (arr[0], arr[i]) = (arr[i], arr[0]);
+            Heapify(arr, i, 0);
+        }
+    }
+
+    static void Heapify(int[] arr, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            (arr[i], arr[largest]) = (arr[largest], arr[i]);
+            Heapify(arr, n, largest);
+        }
+    }
+
+}
