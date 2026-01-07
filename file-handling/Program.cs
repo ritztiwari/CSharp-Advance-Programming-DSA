035eca5b7fe2acd23bea21d1accbfb4d557b68ac
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-07 16:30:11 +0530

    [Rishabh]. Add Program.cs to run all the programs in the file handling project.

---
+++ file-handling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //LineRead.Read();
+        //Console.WriteLine(WordCount.Count("kumar"));
+        //ByteToStream.Stream();
+        WriteOutput.Out();
+    }
+}
