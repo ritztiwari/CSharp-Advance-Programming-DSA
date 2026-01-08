a0db26c2c7b82ac9437d2cdebcc7b759acdce2af
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-08 16:08:03 +0530

    [Rishabh]. Add Program.cs to run all the programs in the runtime analysis project.

---
+++ Runtime-Analysis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //int size = 100000000;
+
+        //int[] arr = new int[size];
+
+        //for(int i = 0; i < size; i++)
+        //{
+        //    arr[i] = i;
+        //}
+
+        //SearchTargetCompare.SearchCompare(arr, size - 1);
+
+        //Random r = new Random();
+
+        //int size = 10000;
+
+        //int[] arr = new int[size];
+
+        //for (int i = 0; i < size; i++)
+        //{
+        //    arr[i] = r.Next(1, 10000);
+        //}
+
+        //SortingCompare.Compare(arr);
+
+        //StringConcatinationCompare.Compare();
+
+        //FibonacciCompare.Compare(30);
+
+        FileReadingCompare.Compare();
+    }
+}
