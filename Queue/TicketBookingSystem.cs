499551ab83344663da9623499d20908f0567d53d
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-09 14:50:22 +0530

    [Rishabh].Add TicketBookingSystem.cs to book tickets for train,bus and flight tickets using queue & OOPS.

---
+++ Queue/TicketBookingSystem.cs
@@ -0,0 +1,78 @@
+﻿//2.Ticket Booking System Queue
+//Use Case: Ticket requests(flight, train) are processed in sequence.
+//OOP Concepts:
+//● Interface: TicketRequest
+//● Encapsulation: Request queue managed inside a service.
+//● Abstraction & Polymorphism: Works for bus/train/flight with common interface.
+
+using System;
+
+interface ITicketRequest
+{
+    void GetTicket();
+}
+
+class Bus : ITicketRequest
+{
+    string ticket;
+
+    public Bus(string ticket)
+    {
+        this.ticket = ticket;
+    }
+
+    public void GetTicket()
+    {
+        Console.WriteLine($"bus ticket is generated: {ticket}");
+    }
+}
+
+class Train : ITicketRequest
+{
+    string ticket;
+
+    public Train(string ticket)
+    {
+        this.ticket = ticket;
+    }
+
+    public void GetTicket()
+    {
+        Console.WriteLine($"train ticket is generated: {ticket}");
+    }
+}
+
+class Flight : ITicketRequest
+{
+    string ticket;
+
+    public Flight(string ticket)
+    {
+        this.ticket = ticket;
+    }
+
+    public void GetTicket()
+    {
+        Console.WriteLine($"flight ticket is generated: {ticket}");
+    }
+}
+
+class TicketBookingSystem
+{
+    Queue<ITicketRequest> queue = new Queue<ITicketRequest>();
+
+    public void AddRequest(ITicketRequest request)
+    {
+        queue.Enqueue(request);
+    }
+
+    public void ProcessRequests()
+    {
+        while (queue.Count > 0)
+        {
+            ITicketRequest request = queue.Dequeue();
+            request.GetTicket();
+        }
+    }
+
+}
