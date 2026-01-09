5a50fc80035559132c974644def27c5cfc4a89e3
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-09 12:18:02 +0530

    [Rishabh]. Add DrawingApp.cs program implements drawing shapes and undoing that using OOPs concept.

---
+++ Stack/DrawingApp.cs
@@ -0,0 +1,63 @@
+﻿// 4.Undo Functionality in Drawing App
+//Use Case: Maintain stack of actions (line drawn, shape added) and support undo.
+//OOP Concepts:
+//● Interface: DrawingAction
+//● Polymorphism: Multiple actions (line, circle, erase) handled uniformly.
+//● Encapsulation: Undo stack wrapped inside the app logic
+ 
+
+using System;
+
+interface IDrawingAction
+{
+    void Undo();
+}
+
+// subclasses implementing IDrawingAction interface 
+class DrawLine : IDrawingAction
+{
+    public void Undo()
+    {
+        Console.WriteLine("Undo Line Drawing");
+    }
+}
+
+class DrawCircle : IDrawingAction
+{
+    public void Undo()
+    {
+        Console.WriteLine("Undo Circle Drawing");
+    }
+}
+
+class Erase : IDrawingAction
+{
+    public void Undo()
+    {
+        Console.WriteLine("Undo Erase Action");
+    }
+}
+
+internal class DrawingApp
+{
+    // stack of actions
+    Stack<IDrawingAction> st = new Stack<IDrawingAction>();
+
+    //performing drawing aciton by using interface
+    public void DoTheDrawing(IDrawingAction s)
+    {
+        st.Push(s);
+        Console.WriteLine("drawing done");
+    }
+
+    public void Undo()
+    {
+        if (st.Count > 0)
+        {
+            IDrawingAction s = st.Pop();
+            s.Undo();
+        }
+        else
+            Console.WriteLine("Nothing to undo");
+    }
+}
