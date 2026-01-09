e42074ca9a79586f0634208628dc9bfcd11a990e
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-09 12:19:15 +0530

    [Rishabh]. Add program.cs to run all the programs that is in stack projects.

---
+++ Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+internal class Program 
+{
+    internal static void Main(string[] args)
+    {
+        IExpressionEvaluator evaluator = new PostfixEvaluator();
+
+        string expr = "(8+2*6)*2";
+        double result = evaluator.Evaluate(expr);
+
+        Console.WriteLine("Result: " + result);
+
+        //DrawingApp app = new DrawingApp();
+        //app.DoTheDrawing(new DrawLine());
+        //app.DoTheDrawing(new Erase());
+        //app.DoTheDrawing(new DrawCircle());
+        //app.Undo();
+        //app.Undo();
+
+        //INavigationManager tab = new NavigationManager();
+        //tab.OpenTab("www.google.com");
+        //tab.OpenTab("www.Netflix.com");
+        //tab.OpenTab("www.Facebook.com");
+        //tab.OpenTab("www.Chatgpt.com");
+        //tab.CurrentTab();
+        //tab.GoBackward();
+        //tab.GoBackward();
+        //tab.GoForward();
+        //tab.CurrentTab();
+
+        //ISyntaxChecker obj = new ValidParenthesis();
+        //Console.WriteLine("Give parentheses notation: ");
+        //string str = Console.ReadLine() ?? "";
+        //bool result = obj.CheckParenthesis(str);
+        //Console.Write(result);
+
+    }
+}
