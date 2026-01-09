3f7d7dacc34af797d39092345dce414e282a8487
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-09 14:51:53 +0530

    [Rishabh].Add Program.cs to run all the program that are in queue project.

---
+++ Queue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        //PrintJobManager manager = new PrintJobManager();
+
+        //manager.AddJobs(new TextDocument("Project Report"));
+        //manager.AddJobs(new ImageFile("Photo.png"));
+        //manager.AddJobs(new TextDocument("Resume"));
+
+        //manager.ProcessPrintWork();
+
+        //TicketBookingSystem service = new TicketBookingSystem();
+
+        //service.AddRequest(new Bus("bus101"));
+        //service.AddRequest(new Train("train102"));
+        //service.AddRequest(new Flight("flight1025"));
+
+        //service.ProcessRequests();
+
+        //CallCenterManagement manager = new CallCenterManagement();
+
+        //manager.AddCalls(new SupportCall());
+        //manager.AddCalls(new SupportCall());
+        //manager.AddCalls(new SalesCall());
+        //manager.AddCalls(new SalesCall());
+        //manager.AddCalls(new SupportCall());
+
+        //manager.ProcessCalls();
+
+        TaskDispatcher task = new TaskDispatcher();
+
+        task.AddTask(new Upload());
+        task.AddTask(new Upload());
+        task.AddTask(new Report());
+        task.AddTask(new Report());
+
+        task.HandleTask();
+    }
+}
